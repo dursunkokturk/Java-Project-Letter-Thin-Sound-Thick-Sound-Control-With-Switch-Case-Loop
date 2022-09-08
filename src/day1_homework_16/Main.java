6f421db3fefd74db33f688caad4e6eb5282d2c22
Dursun Köktürk <dursunserefkokturk@gmail.com>
2022-09-08 12:49:06 +0300

    Letter Thin Sound Thick Sound

---
+++ src/day1_homework_16/Main.java
@@ -0,0 +1,20 @@
+package day1_homework_16;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Girilen Harf Kalin Sesli Mi Ince Sesli Mi
+		char word = 'A';
+		
+		switch(word) {
+		case'A':
+		case'I':
+		case'O':
+		case'U':
+			System.out.println("Kalýn Sesli Harf");
+				break;
+		default:
+			System.out.println("Ýnce Sesli Harf");
+		}
+	}
+}
